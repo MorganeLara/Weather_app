e7421ce7160727335024dc0f8bfa80796768d2c5
MorganeLara <morganelara.design@gmail.com>
2022-05-10 14:22:12 +0200

    units weather convertor

--- src/index.js
+++ src/index.js
@@ -84,14 +84,30 @@ function displayTemperature(response) {
   currentButton.addEventListener("click", displayLocation);
 }
 
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
 searchCity("Brussels");
-
-//function displayFahrenheitTemperature(event) {
-//event.preventDefault();
-//let fahrenheitTemperature = (14 * 9) / 5 + 32;
-//alert("Link clicked");
-//alert(fahrenheitTemperature);
-//}
-
-//let fahrenheitLink = document.querySelector("fahrenheit-link");
-//fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
